458fa6a8437ac7647086de7e4af90da96969e4a6
Stacy Bridges <stcybrdgs@gmail.com>
2024-07-26 12:27:41 -0500

    re-arranged splide folders

--- splideCarousel/script.js
+++ splideCarousel/script.js
@@ -1,18 +1,12 @@
 const splideList = document.querySelector('.splide__list')
 
 function getPhotoFileNames() {
-  return [
-    'burger_combo_0.png',
-    'burger_1.jpg',
-    'burger_2.jpg',
-    'chicken_sandwich.png',
-    'chik_fil_a_0.png',
-  ]
+  return ['burger_1.jpg', 'burger_2.jpg']
 }
 
 function populateCarousel() {
   const photoFileNames = getPhotoFileNames()
-  const basePath = './images/'
+  const basePath = './public/images/'
 
   photoFileNames.forEach((fileName) => {
     const filePath = basePath.concat(fileName)
